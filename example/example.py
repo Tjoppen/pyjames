ac1e963e1edac18ef05c936a365eb65342aa96f8
Tomas Härdin <tomas.hardin@codemill.se>
2011-07-13 09:56:54 +0200

    Replace example.cpp with example.py and update Makefile

---
+++ example/example.py
@@ -0,0 +1,19 @@
+import sys
+from xml.dom.minidom import *
+from generated.PersonDocument import *
+from generated.PersonListDocument import *
+
+def main():
+    #unmarshal personIn from stdin
+    personIn = PersonDocument.fromXML(sys.stdin)
+
+    #create a list containing personIn and some other person
+    list = PersonListDocument()
+    list.person.append(personIn)
+    list.person.append(PersonType("Some Otherguy", "Somewhere 999", 1985))
+
+    #finally, marshal the list to stdout
+    print list.toxml()
+
+if __name__ == "__main__":
+    main()
